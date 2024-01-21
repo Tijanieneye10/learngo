738fb0f259883ddea30c98ac74fea3f9aa6a9871
Tijani Eneye <brainyworld10@gmail.com>
2024-01-21 20:55:45 +0100

    any

--- main.go
+++ main.go
@@ -4,7 +4,30 @@ import "fmt"
 
 // variables
 
-func main(){
-	
+func main() {
+	printSomething(40)
+	anyParamAndReturn(2)
+	anyParamAndReturn("Hello world")
 }
 
+func printSomething(value any) {
+	intVal, ok := value.(int)
+	if ok {
+		fmt.Println(intVal + 1)
+	}
+}
+
+func anyParamAndReturn(value interface{}) interface{} {
+	intVal, isInt := value.(int)
+	if isInt {
+		fmt.Println(intVal)
+	}
+
+	stringVal, isString := value.(string)
+
+	if isString {
+		fmt.Println(stringVal)
+	}
+
+	return nil
+}
