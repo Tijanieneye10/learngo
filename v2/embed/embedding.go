cee16d415cd7d1b66e5ebeae51c063b11e332b5d
Tijani Eneye <brainyworld10@gmail.com>
2024-01-21 09:45:19 +0100

    embedding struct

--- v2/embed/embedding.go
+++ v2/embed/embedding.go
@@ -1,27 +1,30 @@
-package main
+package embed
 
 import "time"
 
 type User struct {
 	Firstname string
-	Lastname string
-	Dob time.Time
+	Lastname  string
+	Dob       time.Time
 }
 
 type Admin struct {
-	Email string
+	Email    string
 	Password string
 	User
 }
 
-func NewAdmin(email, password) Admin {
-	Email: email,
-	Password: password,
+func NewAdmin(email, password string) Admin {
+	return Admin{
+		Email:    email,
+		Password: password,
 		User: User{
-		Firstname: "John",
-		Lastname: "Doe",
-		Dob: time.Now()
-	},
+			Firstname: "John",
+			Lastname:  "Doe",
+			Dob:       time.Now(),
+		},
+	}
+
 }
 
 func main() {
